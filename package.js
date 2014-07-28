38336a4eba30652de4e839a6b7feb3b17df17c6c
Linan Qiu <linanqiu@gmail.com>
2014-07-28 11:32:44 +0800

    typo

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
 	summary: "A package for getting user data and friends from a Facebook user"
 });
 
-Package.on_user(function(api, where) {
+Package.on_use(function(api, where) {
 	api.user(['minimongo', 'mongo-livedata', 'templating'], 'client');
 
 	api.add_files(['facebookPermissions.js'], 'client');
