3de2ad4d3d367accffdbbfaef308687e8f720750
Linan Qiu <linanqiu@gmail.com>
2014-07-28 11:42:51 +0800

    deps

--- package.js
+++ package.js
@@ -2,8 +2,12 @@ Package.describe({
 	summary: "A package for getting user data and friends from a Facebook user"
 });
 
+Npm.depends({
+	'fb-graph': '0.2.10'
+});
+
 Package.on_use(function(api, where) {
-	api.use(['minimongo', 'mongo-livedata'], 'client');
+	api.use(['minimongo', 'mongo-livedata', 'accounts-facebook', 'accounts-ui'], 'client');
 
 	api.add_files(['facebookPermissions.js'], 'client');
 	api.add_files(['facebook.js'], 'server')
